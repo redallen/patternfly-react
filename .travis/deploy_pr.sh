eaaf443018de9a03e349f1dbd5d84f72a7e07c4e
zack allen <zallen@redhat.com>
2019-02-05 15:26:16 -0500

    use tr instead of sed

--- .travis/deploy_pr.sh
+++ .travis/deploy_pr.sh
@@ -33,12 +33,11 @@ fi
 for DEPLOY_SUBDOMAIN_UNFORMATTED in "${DEPLOY_SUBDOMAIN_UNFORMATTED_LIST[@]}"
 do
   echo $DEPLOY_SUBDOMAIN_UNFORMATTED
-  # replaces "/" or "." with "-"
-  # sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
+  # replaces "/" or "." with "-", ref http://stackoverflow.com/a/2871217/689223
   # Domain names follow the RFC1123 spec [a-Z] [0-9] [-]
   # The length is limited to 253 characters
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
-  DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
+  DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | tr '[\/\.]' '-'`
   DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
   echo 'Deploy domain variable: ' ${DEPLOY_DOMAIN}
   export SURGE_TOKEN=`echo $SURGE_ENC | rev | base64 --decode`
