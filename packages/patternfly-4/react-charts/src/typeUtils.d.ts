e56e15d3c7bba80231a711112faad3be66643b77
zack allen <zallen@redhat.com>
2019-02-07 11:41:35 -0500

    fix(thing): example fix

--- packages/patternfly-4/react-charts/src/typeUtils.d.ts
+++ packages/patternfly-4/react-charts/src/typeUtils.d.ts
@@ -4,5 +4,7 @@ export type OneOf<T, K extends keyof T> = T[K];
 export type Omit<T, K extends keyof T> = Pick<
   T,
   ({ [P in keyof T]: P } &
-    { [P in K]: never } & { [x: string]: never; [x: number]: never })[keyof T]
+    { [P in K]: never } & { [x: string]: never;[x: number]: never })[keyof T]
 >;
+
+// example
